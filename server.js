6187a78eddca7ac52271a27d9989d637c7f08e92
Mateusz Kardaś <mateusz.kardas@kiwijobs.pl>
2020-06-04 17:29:19 +0200

    remove unnecessary instances

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { hostname } = require('os');
 const http = require('http');
-const message = `Hello World v9 from ${hostname()} \n`;
+const message = `Hello World v10 from ${hostname()} \n`;
 const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
