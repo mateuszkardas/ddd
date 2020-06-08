5f3d37f54f8ca9a34eeecd927eca0fb49354b570
Mateusz Kardaś <mateusz.kardas@kiwijobs.pl>
2020-06-08 20:46:03 +0200

    Split out staging nested stack

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { hostname } = require('os');
 const http = require('http');
 const STACK_NAME = process.env.STACK_NAME || 'Unknown Stack';
-const message = `Hello World v11 from ${hostname()} in ${STACK_NAME}\n`;
+const message = `Hello World v12 from ${hostname()} in ${STACK_NAME}\n`;
 const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
